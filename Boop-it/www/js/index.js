df24c9195a51b5683c27b2b48d89174f05a4a07e
Arrieta <albert.arrieta@uleth.ca>
2022-11-30 12:05:06 -0700

    if statements work

--- Boop-it/www/js/index.js
+++ Boop-it/www/js/index.js
@@ -8,32 +8,32 @@ function onDeviceReady() {
         alert("sorry your browser does not support this");
     }
 
-    var movebox = 0;
+    //var movebox = 0;
 
     function handleMotion(event) {
         var z = event.alpha;
         var x = event.beta;
         var y = event.gamma;
 
-        $("#z").text("z: " + z);
+        //$("#z").text("z: " + z);
         $("#x").text("x: " + x);
-        $("#y").text("y: " + y);
+        //$("#y").text("y: " + y);
 
-        movebox += x/2
+        actionCheck();
 
-        // $(".box").css("transform", "rotateZ(" + z + "deg) rotateX(" + x + "deg) rotateY(" + y + "deg)");
-        $(".box").css("transform", "rotateZ(" + z + "deg) rotateX(" + x + "deg) rotateY(" + y + "deg) translateY(" + movebox + "px)");
-        // $(".box").css("transform", "rotateZ(" + z + "deg) rotateX(" + x + "deg) rotateY(" + y + "deg) translateY(" + movebox + "px)");
+        function actionCheck() {
+            if (x < 115 && x >= 60) {
+                $(".box").css("background-color", "green");
+            } else {
+                $(".box").css("background-color", "blue");
+            }
 
-        Number.prototype.map = function (in_min, in_max, out_min, out_max) {
-            return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+            console.log(x);
+            
         }
 
-        var r = z.map(0, 360, 0, 255);
-        var g = z.map(-180, 180, 0, 255);
-        var b = z.map(-90, 90, 0, 255);
 
-        $("body").css("background", "rgb(" + r + "," + g + "," + b + ")")
-    }
+       
+    }      
 
 }
